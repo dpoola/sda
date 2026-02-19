a3793627cc8f9740bc5d40d18cfe531522784e47
daniel popoola <23644168@stu.mmu.ac.uk>
2026-02-19 14:42:56 +0000

    .

--- week2lab/src/Main.java
+++ week2lab/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        DiceShaker shaker = new RandomSingleDiceShaker();
+        System.out.format("Shake %d%n", shaker.next());
     }
 }
