8ddc8d746d94b8fcc588ec271ff35760d55c5cad
daniel popoola <23644168@stu.mmu.ac.uk>
2026-02-25 16:53:47 +0000

    .

--- week2lab/src/FixedSingleDiceShaker.java
+++ week2lab/src/FixedSingleDiceShaker.java
@@ -1,6 +1,13 @@
 public class FixedSingleDiceShaker implements DiceShaker{
+
+    private final int[] shakes = new int[]{
+            1, 2, 3, 4, 5, 6
+    };
+
+    private int index;
+
     @Override
     public int next() {
-        return 0;
+        return shakes[index++];
     }
 }
