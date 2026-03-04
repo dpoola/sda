dde2459a2fddb85fc9f7f918a89fea2981998af0
daniel popoola <23644168@stu.mmu.ac.uk>
2026-03-04 14:37:44 +0000

    .

--- week2lab/src/DoubleDiceShaker.java
+++ week2lab/src/DoubleDiceShaker.java
@@ -1,6 +1,14 @@
+import java.util.Random;
+
 public class DoubleDiceShaker implements DiceShaker{
     @Override
     public int next() {
-        return new RandomSingleDiceShaker().next()+ new RandomSingleDiceShaker().next();
+        return new RandomSingleDiceShaker().next()
+                + new RandomSingleDiceShaker().next();
     }
+    @Override
+    public boolean hasNext(){
+        return true;
+    }
+
 }
--- week2lab/src/FixedSingleDiceShaker.java
+++ week2lab/src/FixedSingleDiceShaker.java
@@ -8,11 +8,13 @@ public class FixedSingleDiceShaker implements DiceShaker{
 
     @Override
     public boolean hasNext(){
+
         return index < shakes.length;
     }
 
     @Override
     public int next() {
+
         return shakes[index++];
     }
 }
